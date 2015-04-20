eb7437006226253156c95223faa9663f3656024c
Devtr0n <richardsmailbox@gmail.com>
2015-04-20 11:23:51 -0500

    initial upload of script files (.ps1)

---
+++ GetLinesOfCode-count.ps1
@@ -0,0 +1,4 @@
+#this script measures lines of code, excluding empty lines and comments.
+#last write time is greater than 02/01/2015
+$dir = 'C:\Workspace\mySolution\myProject\myFolder'
+(dir $dir -include *.* -recurse | get-childitem –recurse | where-object {$_.lastwritetime -gt “2/01/2015”} | select-string "^(\s*)//" -notMatch | select-string "^(\s*)$" -notMatch).Count
