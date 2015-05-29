e3b81cc0abe491d5862318e04bb7c74f2924fea1
Richard Hollon <richard@doublelinepartners.com>
2015-05-29 14:01:34 -0500

    Update ReplaceFileNamePattern.ps1
    added comments & header info

--- ReplaceFileNamePattern.ps1
+++ ReplaceFileNamePattern.ps1
@@ -1,1 +1,7 @@
-﻿dir 'C:\Workspace\Project\' | gci | rename-item -newname { $_.name -replace 'Bert','Ernie' }
+<# 
+Author: Richard Hollon - www.richardhollon.com/Contact.aspx
+
+This script renames items in a directory by replacing a string pattern with a new string value.
+
+#>
+dir 'C:\Workspace\Project\' | gci | rename-item -newname { $_.name -replace 'Bert','Ernie' }
