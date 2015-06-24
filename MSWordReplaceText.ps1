98bb395c1f1714c991ef2e9bff7c840ca185b71a
unknown <rhollon@rhollon.tea.state.tx.us>
2015-06-24 10:23:17 -0500

    reformatting of yucky syntax

--- MSWordReplaceText.ps1
+++ MSWordReplaceText.ps1
@@ -15,41 +15,41 @@ trap {'You did not enter a valid target date!'; continue}
 }
 
 #2.) Initialize all variables
-$findText = @("{targetLabel}","{previousLabel}","{productionLabel}","{releaseDate}")
-$userInput = @($targetLabel,$previousLabel,$productionLabel,$releaseDate)
-$templateFileURL='http://myserver/templates/Template.MyProject.PromotionRequest.Test.docx'
-$outFilePath=[Environment]::GetFolderPath("Desktop")+'\MyProject.PromotionRequest.Test.docx'
+$findText 			= @("{targetLabel}","{previousLabel}","{productionLabel}","{releaseDate}")
+$userInput			= @($targetLabel,$previousLabel,$productionLabel,$releaseDate)
+$templateFileURL	= 'http://myserver/templates/Template.MyProject.PromotionRequest.Test.docx'
+$outFilePath		= [Environment]::GetFolderPath("Desktop")+'\MyProject.PromotionRequest.Test.docx'
 
 #3.) Open CEMS Sharepoint template document via Word Interop
-$word = New-Object -com word.application
-$document = $word.Documents.Open($templateFileURL)
-$word.Visible=$False #make this true to watch Word process changes in real-time! 
+$word 				= New-Object -com word.application
+$document 			= $word.Documents.Open($templateFileURL)
+$word.Visible		= $False #make this true to watch Word process changes in real-time! 
 #Write-Host $document.Range().text #this is all the text in the word doc!
 
 #4.) Replace target text in Word document (make the desired changes).
 foreach ($item in $findText) {
-    Replace-Word-Text -FindText $item -ReplaceText $userInput[$findText.IndexOf($item)]
+	Replace-Word-Text -FindText $item -ReplaceText $userInput[$findText.IndexOf($item)]
 }
 
 #5.) Close MS-Word and Document
 $document.Close()
-$Word.Quit()
+$word.Quit()
 
 #======================================================================================================================================
 function Replace-Word-Text(
 [string]$FindText,
 [string]$ReplaceText
 ){
-    $ReplaceAll = 2
-    $FindContinue = 1
-    $MatchCase = $False
-    $MatchWholeWord = $True
-    $MatchWildcards = $False
-    $MatchSoundsLike = $False
-    $MatchAllWordForms = $False
-    $Forward = $True
-    $Wrap = $FindContinue
-    $Format = $False
+    $ReplaceAll 		= 2
+    $FindContinue 		= 1
+    $MatchCase 			= $False
+    $MatchWholeWord 	= $True
+    $MatchWildcards 	= $False
+    $MatchSoundsLike 	= $False
+    $MatchAllWordForms 	= $False
+    $Forward 			= $True
+    $Wrap 				= 1
+    $Format 			= $False
 
     $word.Selection.Find.Execute(
         $FindText,
@@ -62,6 +62,6 @@ function Replace-Word-Text(
         $ReplaceText,
         $ReplaceAll) 
 
-    $document.SaveAs([ref]$outFilePath)
+    $document.SaveAs([ref]$outFilePath) #Save to user's Desktop
 }
 #======================================================================================================================================
