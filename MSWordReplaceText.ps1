c219f2da66ff8b1490f7caa8ce3fffbc8cd9499e
Devtr0n <richardsmailbox@gmail.com>
2015-06-24 14:42:02 -0500

    added hyperlink editing section

--- MSWordReplaceText.ps1
+++ MSWordReplaceText.ps1
@@ -1,41 +1,9 @@
 <# 
 	Author: Richard Hollon - www.richardhollon.com/Contact.aspx
-	This script downloads an MS Word document and replaces targeted text values with user input data.
+	
+	#This script downloads an MS Word document and replaces targeted text values (and hyperlinks) with user input data.
 #>
-#======================================================================================================================================
-
-#1.) Get User Input
-$targetLabel		= Read-Host 'Enter the target release label: '
-$previousLabel		= Read-Host 'Enter the previous release label: '
-$productionLabel	= Read-Host 'Enter the production release label: '
-$releaseDate 		= Read-Host 'Enter the target release date: '
-trap {'You did not enter a valid target date!'; continue}
-. {
-  $releaseDate = [DateTime]::Parse($releaseDate)
-}
-
-#2.) Initialize all variables
-$findText 			= @("{targetLabel}","{previousLabel}","{productionLabel}","{releaseDate}")
-$userInput			= @($targetLabel,$previousLabel,$productionLabel,$releaseDate)
-$templateFileURL	= 'http://myserver/templates/Template.MyProject.PromotionRequest.Test.docx'
-$outFilePath		= [Environment]::GetFolderPath("Desktop")+'\MyProject.PromotionRequest.Test.docx'
-
-#3.) Open CEMS Sharepoint template document via Word Interop
-$word 				= New-Object -com word.application
-$document 			= $word.Documents.Open($templateFileURL)
-$word.Visible		= $False #make this true to watch Word process changes in real-time! 
-#Write-Host $document.Range().text #this is all the text in the word doc!
-
-#4.) Replace target text in Word document (make the desired changes).
-foreach ($item in $findText) {
-	Replace-Word-Text -FindText $item -ReplaceText $userInput[$findText.IndexOf($item)]
-}
-
-#5.) Close MS-Word and Document
-$document.Close()
-$word.Quit()
-
-#======================================================================================================================================
+#==================================================================================================================================
 function Replace-Word-Text(
 [string]$FindText,
 [string]$ReplaceText
@@ -52,16 +20,55 @@ function Replace-Word-Text(
     $Format 			= $False
 
     $word.Selection.Find.Execute(
-        $FindText,
-        $MatchCase,
-        $MatchWholeWord,
-        $MatchWildcards,
-        $MatchSoundsLike,
-        $MatchAllWordForms,
-        $Forward,$Wrap,$Format,
-        $ReplaceText,
-        $ReplaceAll) 
+						$FindText,
+						$MatchCase,
+						$MatchWholeWord,
+						$MatchWildcards,
+						$MatchSoundsLike,
+						$MatchAllWordForms,
+						$Forward,$Wrap,$Format,
+						$ReplaceText,
+						$ReplaceAll) 
 
     $document.SaveAs([ref]$outFilePath) #Save to user's Desktop
 }
-#======================================================================================================================================
+#=================================================================================================================================
+#1.) Get User Input
+$formatDate 			= Get-Date 
+$formatDate 			= $formatDate.ToString('MMMM dd, yyyy hh:mm tt') 
+$targetLabel			= Read-Host 'Enter the target release label'
+$previousLabel			= Read-Host 'Enter the previous release label'
+$productionLabel		= Read-Host 'Enter the production release label'
+$releaseDate 			= Read-Host 'Enter the target release date (ex.' $formatDate')' 
+
+#2.) Initialize all variables
+$findText 				= @("{targetLabel}","{previousLabel}","{productionLabel}","{releaseDate}")
+$releaseDate 			= [DateTime]::Parse($releaseDate)
+$releaseDate 			= $releaseDate.ToString('MMMM dd, yyyy {0} hh:mm tt') -f 'at'
+$userInput				= @($targetLabel,$previousLabel,$productionLabel,$releaseDate)
+$templateFileURL		= 'http://myserver/templates/Template.MyProject.PromotionRequest.Test.docx'
+$outFilePath			= [Environment]::GetFolderPath("Desktop")+'\MyProject.PromotionRequest.Test.docx'
+
+#3.) Open CEMS SharePoint template document via Word Interop
+$word 					= New-Object -com word.application
+$document 				= $word.Documents.Open($templateFileURL)
+$word.Visible			= $True #make this true to watch Word process changes in real-time! 
+
+#4.) Replace target text in Word document (make the desired changes).
+foreach ($item in $findText) {
+	Replace-Word-Text -FindText $item -ReplaceText $userInput[$findText.IndexOf($item)]
+}
+
+#5.) Apply the build labels to the document's Hyperlink URLs
+foreach ($link in $document.Hyperlinks) {
+			
+	if ($link.Address -Match '{targetLabel}') {
+	    $link.Address = $link.Address.Replace('{targetLabel}', $targetLabel)
+        $document.SaveAs([ref]$outFilePath) #Save to user's Desktop
+	}
+}
+
+#6.) Close MS-Word and Document
+$document.Close()
+$word.Quit()
+#=================================================================================================================================
